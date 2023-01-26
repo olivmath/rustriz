645f4f8b1a8b85eaeb708170f85584f271161fc4
Lucas Oliveira <olivmath@protonmail.com>
2023-01-25 22:38:40 -0300

    update: rename matrix_3x3

--- src/domain/matrix.rs
+++ src/domain/matrix.rs
@@ -3,26 +3,28 @@ use std::fmt;
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, PartialEq, Eq, Debug, Copy, Clone)]
-pub struct Matrix {
+pub struct Matrix_3x3 {
     data: [[i32; 3]; 3],
 }
 
-impl Matrix {
-    pub fn new(data: [[i32; 3]; 3]) -> Matrix {
-        Matrix { data }
+impl Matrix_3x3 {
+    pub fn new(data: [[i32; 3]; 3]) -> Matrix_3x3 {
+        Matrix_3x3 { data }
     }
-    pub fn transpose(self) -> Matrix {
+
+    pub fn transpose(self) -> Matrix_3x3 {
         let mut data = [[0; 3]; 3];
         for i in 0..3 {
             for j in 0..3 {
                 data[i][j] = self.data[j][i];
             }
         }
-        return Matrix { data };
+        return Matrix_3x3 { data };
+    }
     }
 }
 
-impl fmt::Display for Matrix {
+impl fmt::Display for Matrix_3x3 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "[\n");
         for i in &self.data {
@@ -37,12 +39,12 @@ impl fmt::Display for Matrix {
 }
 
 mod test {
-    use super::Matrix;
+    use super::Matrix_3x3;
 
     #[test]
-    fn test_transpose_3x3() {
-        let matrix = Matrix::new([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
-        let matrix_t = Matrix::new([[1, 4, 7], [2, 5, 6], [3, 6, 9]]);
+    fn test_transpose() {
+        let matrix = Matrix_3x3::new([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+        let matrix_t = Matrix_3x3::new([[1, 4, 7], [2, 5, 8], [3, 6, 9]]);
         assert_eq!(matrix.transpose(), matrix_t)
     }
 }
