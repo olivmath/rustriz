13c7261629c6cbdbc05c8e2f6322ef9ef246e720
Lucas Oliveira <olivmath@protonmail.com>
2023-01-25 23:07:46 -0300

    add: multiply matrices

--- src/domain/matrix.rs
+++ src/domain/matrix.rs
@@ -6,6 +6,18 @@ pub struct Matrix3x3 {
     pub data: [[i32; 3]; 3],
 }
 
+#[derive(Deserialize, Serialize, PartialEq, Eq, Debug, Copy, Clone)]
+pub struct Matrices {
+    pub a: Matrix3x3,
+    pub b: Matrix3x3,
+}
+
+impl Matrices {
+    pub fn new(a: Matrix3x3, b: Matrix3x3) -> Matrices {
+        Matrices { a, b }
+    }
+}
+
 impl Matrix3x3 {
     pub fn new(data: [[i32; 3]; 3]) -> Matrix3x3 {
         Matrix3x3 { data }
@@ -20,6 +32,17 @@ impl Matrix3x3 {
         }
         return Matrix3x3 { data };
     }
+
+    pub fn mul(self, other: Matrix3x3) -> Matrix3x3 {
+        let mut result = [[0; 3]; 3];
+        for row in 0..3 {
+            for column in 0..3 {
+                for i in 0..3 {
+                    result[row][column] += self.data[row][i] * other.data[i][column];
+                }
+            }
+        }
+        Matrix3x3::new(result)
     }
 }
 
@@ -45,5 +68,14 @@ mod test {
         let matrix = Matrix3x3::new([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
         let matrix_t = Matrix3x3::new([[1, 4, 7], [2, 5, 8], [3, 6, 9]]);
         assert_eq!(matrix.transpose(), matrix_t)
+    }
+
+    #[test]
+    fn test_matrices() {
+        let a = Matrix3x3::new([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+        let b = Matrix3x3::new([[2, 2, 2], [2, 2, 2], [2, 2, 2]]);
+        let matrices = Matrices::new(a, b);
+        let result = Matrix3x3::new([[12, 12, 12], [30, 30, 30], [48, 48, 48]]);
+        assert_eq!(matrices.a.mul(matrices.b.transpose()), result);
     }
 }
--- src/routes/handlers.rs
+++ src/routes/handlers.rs
@@ -1,13 +1,40 @@
-use crate::domain::matrix::Matrix_3x3;
+use crate::domain::matrix::{Matrices, Matrix3x3};
+use hyper::StatusCode;
 use hyper::{Body, Request, Response};
-use hyper::{Method, StatusCode};
+use serde::Deserialize;
 use serde_json::Error;
 use std::convert::Infallible;
 
-async fn req_to_matrix(req: Request<Body>) -> Result<Matrix_3x3, Error> {
+async fn req_to_type<T: for<'a> Deserialize<'a>>(req: Request<Body>) -> Result<T, Error> {
     let body_bytes = hyper::body::to_bytes(req.into_body()).await.unwrap();
     let body_str = String::from_utf8(body_bytes.to_vec()).unwrap();
     serde_json::from_str(&body_str)
+}
+
+pub async fn multiply(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let matrices: Matrices = match req_to_type(req).await {
+        Ok(m) => m,
+        Err(_) => {
+            return Ok(Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body("Invalid Request Body".into())
+                .unwrap())
+        }
+    };
+
+    let res = match serde_json::to_string(&matrices.a.mul(matrices.b)) {
+        Ok(s) => s,
+        Err(e) => {
+            return Ok(Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(format!("Internal Server Error: {:?}", e).into())
+                .unwrap())
+        }
+    };
+    Ok(Response::builder()
+        .status(StatusCode::OK)
+        .body(res.into())
+        .unwrap())
 }
 
 pub async fn transpose(req: Request<Body>) -> Result<Response<Body>, Infallible> {
@@ -43,7 +70,8 @@ pub async fn root() -> Result<Response<Body>, Infallible> {
         .unwrap())
 }
 
-pub async fn not_found() -> Result<Response<Body>, Infallible> {
+pub async fn not_found(url: &str) -> Result<Response<Body>, Infallible> {
+    dbg!(url);
     Ok(Response::builder()
         .status(StatusCode::NOT_FOUND)
         .body("404 Not Found".into())
