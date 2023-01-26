9bcaf017425ea0fec7fab7ce2473a96e07a4be6d
Lucas Oliveira <olivmath@protonmail.com>
2023-01-25 22:37:47 -0300

    update: extract conversion

--- src/routes/handlers.rs
+++ src/routes/handlers.rs
@@ -1,36 +1,39 @@
-use crate::domain::matrix::Matrix;
+use crate::domain::matrix::Matrix_3x3;
 use hyper::{Body, Request, Response};
 use hyper::{Method, StatusCode};
-use serde_json;
+use serde_json::Error;
 use std::convert::Infallible;
 
+async fn req_to_matrix(req: Request<Body>) -> Result<Matrix_3x3, Error> {
+    let body_bytes = hyper::body::to_bytes(req.into_body()).await.unwrap();
+    let body_str = String::from_utf8(body_bytes.to_vec()).unwrap();
+    serde_json::from_str(&body_str)
+}
+
 pub async fn transpose(req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    {
-        let body_bytes = hyper::body::to_bytes(req.into_body()).await.unwrap();
-        let body_str = String::from_utf8(body_bytes.to_vec()).unwrap();
-        let matrix: Matrix = match serde_json::from_str(&body_str) {
-            Ok(m) => m,
-            Err(_) => {
-                return Ok(Response::builder()
-                    .status(StatusCode::BAD_REQUEST)
-                    .body("Invalid Request Body".into())
-                    .unwrap())
-            }
-        };
-        let res = match serde_json::to_string(&matrix.transpose()) {
-            Ok(s) => s,
-            Err(e) => {
-                return Ok(Response::builder()
-                    .status(StatusCode::INTERNAL_SERVER_ERROR)
-                    .body(format!("Internal Server Error: {:?}", e).into())
-                    .unwrap())
-            }
-        };
-        Ok(Response::builder()
-            .status(StatusCode::OK)
-            .body(res.into())
-            .unwrap())
-    }
+    let matrix: Matrix_3x3 = match req_to_matrix(req).await {
+        Ok(m) => m,
+        Err(_) => {
+            return Ok(Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body("Invalid Request Body".into())
+                .unwrap())
+        }
+    };
+
+    let res = match serde_json::to_string(&matrix.transpose()) {
+        Ok(s) => s,
+        Err(e) => {
+            return Ok(Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(format!("Internal Server Error: {:?}", e).into())
+                .unwrap())
+        }
+    };
+    Ok(Response::builder()
+        .status(StatusCode::OK)
+        .body(res.into())
+        .unwrap())
 }
 
 pub async fn root() -> Result<Response<Body>, Infallible> {
